79e7a79a0e1abec743040678b5aefa3e20b6fa20
Philip Reinhart <74872363+philiprein@users.noreply.github.com>
2023-12-04 20:59:11 +0100

    fix shellenv bug, reformat code

--- 5a_homebrew.sh
+++ 5a_homebrew.sh
@@ -1,9 +1,8 @@
 #!/bin/zsh
 
-
 # prompt for sudo password at the beginning of the script
 if sudo -v; then
-  while true; do 
+  while true; do
     # keep-alive for sudo
     sudo -n true
     sleep 60
@@ -11,39 +10,42 @@ if sudo -v; then
   done 2>/dev/null &
 fi
 
+# check if homebrew is already installed
+if ! command -v brew &>/dev/null; then
+  # not installed
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-install_homebrew() {
-  # check if homebrew is already installed
-  if ! command -v brew &> /dev/null; then
-    # not installed
-    echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    if [[ $(uname -m | grep arm) != "" ]]; then
-      # make homebrew shell environment available on zsh and bash shells
-      echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' | tee -a $HOME/.zprofile >> $HOME/.bash_profile
-      eval "$(/opt/homebrew/bin/brew shellenv)"
-    else
-      :
-    fi
-
-    # turn off analytics
-    brew analytics off
+  if [[ $(uname -m | grep arm) != "" ]]; then
+    # make homebrew shell environment available on zsh and bash shells
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' | tee -a $HOME/.zprofile >>$HOME/.bash_profile
+    # scripts are run in subshells, so this has no effect on the shell environment in which the script is run
+    #eval "$(/opt/homebrew/bin/brew shellenv)"
   else
-    # installed
-    echo "Homebrew is already installed. Skipping install..."
+    :
   fi
 
-  # update homebrew
-  echo "Updating Homebrew..."
-  brew update && brew upgrade
+  # turn off analytics
+  brew analytics off
+else
+  # installed
+  echo "Homebrew is already installed. Skipping install..."
+fi
 
-  # cleanup
-  echo "Cleaning up Homebrew..."
-  brew cleanup --prune=all 1> /dev/null
+# update homebrew
+echo "Updating Homebrew..."
+brew update && brew upgrade
 
-  # check if everything works as expected
-  brew doctor
-}
+# cleanup
+echo "Cleaning up Homebrew..."
+brew cleanup --prune=all 1>/dev/null
 
-install_homebrew
+# check if everything works as expected
+brew doctor
+
+echo "Finished installing and updating Homebrew."
+if [[ $(uname -m | grep arm) != "" ]]; then
+  echo "Run 'eval "$(/opt/homebrew/bin/brew shellenv)"' to set the shell environment."
+else
+  :
+fi
